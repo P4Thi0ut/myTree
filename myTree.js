d2c6f4fc95dbd554a6f3b362e296a571266a9b16
Pierre-Alain Thiout <pierre-alain.thiout@epita.fr>
2015-11-01 14:55:33 +0100

    Tree Draft

--- myTree.js
+++ myTree.js
@@ -22,41 +22,48 @@ myTree.directive('tree', ['$timeout', function($timeout) {
 			$timeout(function() {
 				scope.tree = ngModelCtrl.$viewValue;
 				for (n in scope.tree.data)
-					process(0, scope.tree.data[n], 0);
+					process(0, scope.tree.data[n], 0, 0, 0);
 				scope.load = true;
 				console.info(scope.draw);
 				//console.info(scope.tree);
 			},0);
 
-			var process = function(level, node, index) {
+			var process = function(level, node, index, parentx, parenty) {
 				var px = parseInt(index, 10) * 70;
-				var py = parseInt(level, 10) * 50;
+				var py = parseInt(level, 10) * 60;
+				//top line
 				var lsrcx = px + 35;
 				var lsrcy = py;
 				var ldstx = lsrcx;
 				var ldsty = py - 20;
+				//bottom line
+				var blsrcx = px + 35;
+				var blsrcy = py + 20;
+				var bldstx = blsrcx;
+				var bldsty = blsrcy + 10;
+
+				var n = {
+					title: node.title,
+					posx: px,
+					posy: py,
+					linesrcx: lsrcx,
+					linesrcy: lsrcy,
+					linedstx: ldstx,
+					linedsty: ldsty,
+					blinesrcx: blsrcx,
+					blinesrcy: blsrcy,
+					blinedstx: bldstx,
+					blinedsty: bldsty,
+					hasChilds: (node.childs.length > 0),
+					parentx: parentx,
+					parenty: parenty
+				};
 				if (level in scope.draw)
-					scope.draw[level].push({
-						title: node.title,
-						posx: px,
-						posy: py,
-						linesrcx: lsrcx,
-						linesrcy: lsrcy,
-						linedstx: ldstx,
-						linedsty: ldsty
-					});
+					scope.draw[level].push(n);
 				else
-					scope.draw[level] = [{
-						title: node.title,
-						posx: px,
-						posy: py,
-						linesrcx: lsrcx,
-						linesrcy: lsrcy,
-						linedstx: ldstx,
-						linedsty: ldsty
-					}];
+					scope.draw[level] = [n];
 				for (c in node.childs)
-					process(level + 1, node.childs[c], index + parseInt(c, 10));
+					process(level + 1, node.childs[c], index + parseInt(c, 10), bldstx, bldsty);
 			}
 		}
 	}
