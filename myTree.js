6a3c442630df7e5d5857f5e27706d0efd2f18fb7
Pierre-Alain Thiout <pierre-alain.thiout@epita.fr>
2015-11-02 21:13:22 +0100

    Adding dashed link and arrow relationship

--- myTree.js
+++ myTree.js
@@ -1,6 +1,8 @@
 var myTree = angular.module('myTree', []);
 
-angular.forEach(['r', 'x', 'y', 'x1', 'x2', 'y1', 'y2', 'fill', 'width', 'height', 'xlink', 'href'], function(name) {
+angular.forEach(['r', 'x', 'y', 'x1', 'x2', 'y1', 'y2',
+				'fill', 'width', 'height', 'xlink', 'href',
+				'cx', 'cy', 'r', 'd', 'points'], function(name) {
   	var ngName = 'ng' + name[0].toUpperCase() + name.slice(1);
   	myTree.directive(ngName, function() {
     	return function(scope, element, attrs) {
@@ -21,31 +23,44 @@ myTree.directive('tree', ['$timeout', function($timeout) {
 			scope.draw = {};
 			$timeout(function() {
 				scope.tree = ngModelCtrl.$viewValue;
+				scope.tree.options.radius = (scope.tree.options.nodeW < scope.tree.options.nodeH) ?
+					(scope.tree.options.nodeW / 2):(scope.tree.options.nodeH / 2);
 				for (n in scope.tree.data)
 					process(0, scope.tree.data[n], 0, 0, 0, scope.tree.options);
 				scope.load = true;
+				//console.info(scope.draw);
 			},0);
 
 			var process = function(level, node, index, parentx, parenty) {
-				var px = parseInt(index, 10) * (scope.tree.options.nodeW * 1.5);
-				var py = parseInt(level, 10) * (scope.tree.options.nodeH * 2);
+				level = parseInt(level, 10);
+				//var nbrNodes = scope.draw[level].length;
+				var px = /*(scope.tree.options.width / 2) +*/ parseInt(index, 10) * (scope.tree.options.nodeW * 1.5);
+				var py = level * (scope.tree.options.nodeH * 2);
 				//top lines
-				var lsrcx = px + 30;
+				var lsrcx = px + (scope.tree.options.nodeW / 2);
 				var lsrcy = py;
 				var ldstx = lsrcx;
 				var ldsty = py - (scope.tree.options.nodeH / 2);
 				//bottom line
-				var blsrcx = px + 30;
+				var blsrcx = px + (scope.tree.options.nodeW / 2);
 				var blsrcy = py + scope.tree.options.nodeH;
 				var bldstx = blsrcx;
 				var bldsty = blsrcy + (scope.tree.options.nodeH / 2);
+				//circle
+				var cx = px + (scope.tree.options.nodeW / 2);
+				var cy = py + (scope.tree.options.nodeH / 2);
 
 				var n = {
 					title: node.title,
 					color: node.color,
 					vignette: ("vignette" in node) ? node.vignette : false,
+					stroke: ("stroke" in node) ? node.stroke : "plain",
+					strokeType: ("strokeType" in node) ? "arrow": false,
+					shape: node.shape,
 					posx: px,
 					posy: py,
+					cx: cx,
+					cy: cy,
 					linesrcx: lsrcx,
 					linesrcy: lsrcy,
 					linedstx: ldstx,
@@ -58,6 +73,12 @@ myTree.directive('tree', ['$timeout', function($timeout) {
 					parentx: parentx,
 					parenty: parenty
 				};
+				if (n.strokeType) {
+					var p1 = n.linesrcx - 5;
+					var p2 = n.linesrcx + 5;
+					var offy = n.linesrcy - 5;
+					n.strPath = n.linesrcx + "," + n.linesrcy + " " + p1 + "," + offy + " " + p2 + "," + offy;
+				}
 				if (level in scope.draw)
 					scope.draw[level].push(n);
 				else
